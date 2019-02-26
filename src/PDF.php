1aca8cfd185ebb9a1278cac30ddcaa2d8f3e20b3
Ratheep <ratheepan93@hotmail.com>
2019-02-26 13:54:05 +0530

    update to php >= 7.0

---
+++ src/PDF.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jeylabs\TCPDI;
+use Jeylabs\TCPDI\TCPDI;
+
+class PDF extends TCPDI
+{
+    var $_tplIdx;
+    var $numPages;
+
+    function Header()
+    {
+    }
+
+    function Footer()
+    {
+    }
+}
